503318ab894477cd73a51d4045dfe0df298f5621
SnowStorm128 <buiw17716@gmail.com>
2025-07-23 16:52:35 +0700

    Added redis as caching server.

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,19 @@ import debugPkg from 'debug';
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import winston from 'winston';
+import {createClient} from 'redis';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { eq, getTableColumns, sql } from 'drizzle-orm';
 import { userTable } from './db/schema.js';
+import { cli } from 'winston/lib/winston/config/index.js';
 
 // Init variable
 const app = express();
 const catRouter = express.Router();
 const port = 3002;
+const client = await createClient()
+  .on("error", (err) => console.log("Redis Client Error", err))
+  .connect();
 const db = drizzle(process.env.DATABASE_URL!);
 const logger = winston.createLogger({
     format: winston.format.combine(
@@ -47,57 +52,24 @@ app.disable('x-powered-by');
 app.set('view engine', 'ejs');
 app.set('views', process.cwd() + '/views');
 
-function runWorker(workerData){
-    return new Promise((resolve, reject) => {
-        const worker = new Worker("./src/hello.js", {
-            workerData
-        });
-        worker.on("message", resolve);
-        worker.on("error", reject);
-        worker.on("exit", code => {
-            if(code !== 0){
-                reject(new Error(`Worker stopped with exit code ${code}`));
-            }
-        })
-    })
-}
 
 // Cat Route
 catRouter.get('/', async (req, res) => {
-    const { id, ...rest } = getTableColumns(userTable);
+    const currentId = 1;
 
-    const users = await db.select({
-        ...rest
-    }).from(userTable);
-    console.log('Getting all users from the db: ', users);
+    const cached = await client.get('user:' + currentId)
+    if(cached && typeof cached == 'string'){
+        console.log('Getting all users from the cache: ', JSON.parse(cached));
+    } else {
+        const users = await db.select().from(userTable).where(eq(userTable.id, currentId));
+        console.log('Getting all users from the db: ', users);
+        client.set('user:' + currentId, JSON.stringify(users), {
+            EX: 3600,
+        });
+    }
 
-    // const ls = child_process.spawn('ls', ['-lh', '/usr']);
-    // ls.stdout.on('data', (data) => {
-    //     res.write(data.toString())
-    // })
 
-    // ls.stdout.on('close', code => {
-    //     res.end();
-    // })
-
-    // child_process.exec('ls -lh /usr', (error, stdout, stderr) => {
-    //     if (error) {
-    //         console.error(`exec error: ${error}`);
-    //         return;
-    //     }
-    //     res.write(stdout);
-    //     res.write(stderr);
-    //     res.end();
-    // })
-
-    // const hello = child_process.fork("src/hello.js");
-    // hello.send({number: 12})
-    // hello.on('message', message => {
-    //     res.end(message);
-    // })
-
-    const worker1 = await runWorker({number: 12}).then((data : {result: number}) => data.result)
-    res.end(worker1);
+    res.end();
 })
 app.use('/cat', catRouter);
 
@@ -125,8 +97,6 @@ try {
     server.listen(port, () => {
         mainDebug(`Secure server running on https://localhost:${port}`);
     });
-
-
 
 
 } catch (err) {
